6ffcb4307d075d7ce1bf141ab9acdfbf80445d1b
Daniel Petrov <daniel221569@gmail.com>
2020-01-20 19:19:59 +0100

    Evaluate NN based on 100 runs

--- NeuralNetwork.R
+++ NeuralNetwork.R
@@ -2,6 +2,10 @@ install.packages("neuralnet")
 install.packages("boot")
 install.packages("plyr")
 
+library(boot)
+library(plyr)
+library(neuralnet)
+library(gmodels)
 
 rm(list = ls())
 
@@ -24,60 +28,38 @@ data_norm <- as.data.frame(lapply(data, normalize))
 # all data is normalised from 0 to 1
 head(data_norm)
 
-# 4. Data splitting
-data_rows <- nrow(data_norm)
-data_sample <- sample(data_rows, data_rows * 0.8)
-data_train <- data_norm[data_sample,]
-data_test <- data_norm[-data_sample,]
-
-# 5. Model creation (training data)
-library(neuralnet)
-
-# train the model
-data_model <- neuralnet(T1+T2+T3+T4+T5~P1+P2+P3+P4+P5+P6+P7+P8+P9+P10, data = data_train, hidden = c(30, 10))
-
-# plot(data_model)
-
-# 6. Prediction (test data)
-model_results <- compute(data_model, data_test[1:10])
-
-# 7. Model Evaluation
-predicted_alcohol <- model_results$net.result
-
-original_values <- max.col(data_test[, 11:15])
-predicted_values <- max.col(predicted_alcohol)
-mean(predicted_values == original_values)
-
-library(gmodels)
-CrossTable(original_values, predicted_values, prop.chisq = FALSE,
-           prop.r = FALSE, prop.c=FALSE, dnn = c("actual","predicted"))
-
-# K fold cross validation
-
-library(boot)
-library(plyr)
-
-k <- 5
+n <- 100
 outs <- NULL
 
 pbar <- create_progress_bar('text')
-pbar$init(k)
+pbar$init(n)
 
-for(i in 1:k){
-  sample <- sample(1:nrow(data_norm),round(0.8*nrow(data_norm)))
-  train <- data_norm[sample,]
-  test <- data_norm[-sample,]
+for(i in 1:n){
+  # 4. Data splitting
+  set.seed(i)
+  data_rows <- nrow(data_norm)
+  data_sample <- sample(data_rows, data_rows * 0.6)
+  data_train <- data_norm[data_sample,]
+  data_test <- data_norm[-data_sample,]
 
-  data_model <- neuralnet(T1+T2+T3+T4+T5~P1+P2+P3+P4+P5+P6+P7+P8+P9+P10, data = train, hidden = c(30, 10))
+  # 5. Model creation (training data)
+  data_model <- neuralnet(T1+T2+T3+T4+T5~P1+P2+P3+P4+P5+P6+P7+P8+P9+P10, data = data_train, hidden = c(30, 10))
 
-  predict <- compute(data_model, test[,1:10])
+  # 6. Prediction (test data)
+  model_results <- compute(data_model, data_test[1:10])
 
-  predict_results <- predict$net.result
-  original_values <- max.col(test[, 11:15])
-  predict_results <- max.col(predict_results)
-  outs[i] <- mean(predict_results == original_values)
+  # 7. Model Evaluation
+  predicted_alcohol <- model_results$net.result
+
+  original_values <- max.col(data_test[, 11:15])
+  predicted_values <- max.col(predicted_alcohol)
+
+  outs[i] <- mean(predicted_values == original_values)
 
   pbar$step()
+
+  # CrossTable(original_values, predicted_values, prop.chisq = FALSE, prop.r = FALSE, prop.c=FALSE, dnn = c("actual","predicted"))
 }
 
+# average accuracy
 mean(outs)
